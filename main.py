1a3014fa5ea8d3bb19e2f73b1075ff22335acd02
Dilip K N <64477187+dilipkn007@users.noreply.github.com>
2024-05-06 19:00:02 +0000

    added some changes

--- main.py
+++ main.py
@@ -1,24 +1,23 @@
 import reader.configReader as read
-from flask import Flask, render_template, request,  redirect, url_for, flash,send_file
+from flask import Flask, render_template, request, redirect, url_for, flash, send_file
 from flask_login import UserMixin, LoginManager, login_user, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from pymongo import MongoClient
 from reportlab.lib.pagesizes import letter
 from reportlab.pdfgen import canvas
 
-app=Flask(__name__,static_folder='static')
-app.secret_key="dilipkn007"
+app = Flask(__name__, static_folder='static')
+app.secret_key = "dilipkn007"
 app.config['LOGIN_FLAG'] = False
-login =str(app.config['LOGIN_FLAG'])
-U=None
+login = str(app.config['LOGIN_FLAG'])
+U = None
 
 # MongoDB connection
-connection_string=read.config_reader("DB_CONNECTION","string")
+connection_string = read.config_reader("DB_CONNECTION", "string")
 client = MongoClient(connection_string)
 db = client['MovieBooking']
 users_collection = db['users']
 collection = db['movie_data']
-
 
 # User Loader for Login Manager
 login_manager = LoginManager(app)
@@ -26,25 +25,25 @@ login_manager.login_view = 'login'
 
 
 class User(UserMixin):
+
     def __init__(self, user_dict):
         self.id = str(user_dict['_id'])
         self.username = user_dict['username']
         self.email = user_dict['email']
         self.password = user_dict['password']
 
-def create_movie_ticket(ticket_id, movie_name, screen_name, seat_numbers, show_date, show_time, amount):
+
+def create_movie_ticket(ticket_id, movie_name, screen_name, seat_numbers,
+                        show_date, show_time, amount):
     # Create a PDF canvas
     c = canvas.Canvas("movie_ticket.pdf", pagesize=letter)
 
     # Define content for the ticket
     content = [
-        f"Ticket ID: {ticket_id}",
-        f"Movie Name: {movie_name}",
+        f"Ticket ID: {ticket_id}", f"Movie Name: {movie_name}",
         f"Screen Name: {screen_name}",
-        f"Seat Numbers: {', '.join(seat_numbers)}",
-        f"Show Date: {show_date}",
-        f"Show Time: {show_time}",
-        f"Amount: ${amount}"
+        f"Seat Numbers: {', '.join(seat_numbers)}", f"Show Date: {show_date}",
+        f"Show Time: {show_time}", f"Amount: ${amount}"
     ]
 
     # Draw the content on the PDF
@@ -59,6 +58,7 @@ def create_movie_ticket(ticket_id, movie_name, screen_name, seat_numbers, show_d
 
     # Save the PDF
     c.save()
+
 
 # def create_movie_ticket(ticket_id, movie_name, screen_name, seat_numbers, show_date, show_time, amount):
 #     # Create a PDF canvas
@@ -94,7 +94,6 @@ def create_movie_ticket(ticket_id, movie_name, screen_name, seat_numbers, show_d
 # create_movie_ticket(ticket_id, movie_name, screen_name, seat_numbers, show_date, show_time, amount)
 
 
-
 @login_manager.user_loader
 def load_user(user_id):
     user_dict = db.signup.find_one({'email': user_id})
@@ -109,9 +108,9 @@ def index():
     return render_template('index.html', loginstatus=login, current_user=U)
 
 
-@app.route('/signup',methods=['POST','GET'])
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
-    if request.method=='POST':
+    if request.method == 'POST':
         name = request.form['signup_name']
         email = request.form['signup_email']
         password = request.form['signup_password']
@@ -122,22 +121,23 @@ def signup():
             return render_template('/signup.html')
 
         users_collection.insert_one({
-            'username': name,
-            'email': email,
-            'password': generate_password_hash(password)
+            'username':
+            name,
+            'email':
+            email,
+            'password':
+            generate_password_hash(password)
         })
-
 
         flash("Signup Success Please Login", "success")
 
     return render_template('index.html')
 
 
-
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     global U
-    if request.method=='POST':
+    if request.method == 'POST':
 
         username = request.form['username']
         password = request.form['password']
@@ -145,20 +145,22 @@ def login():
 
         if user_dict and check_password_hash(user_dict['password'], password):
             collection.delete_many({})
-            collection.insert_one({"id":username})
+            collection.insert_one({"id": username})
             return render_template('kannadaaction.html')
     return render_template('index.html')
+
 
 @app.route('/theaterlist')
 def theaterlist():
     return render_template('Theater_List.html')
+
 
 @app.route('/Book_Seats')
 def book_seats():
     return render_template('Book_Seats.html')
 
 
-@app.route('/store_data/<id>',methods=['POST'])
+@app.route('/store_data/<id>', methods=['POST'])
 def store_data(id):
     if request.method == 'POST':
         movie = request.form[f'ka_actmv_{id}']
@@ -167,7 +169,8 @@ def store_data(id):
         return render_template('Theater_List.html')
     return render_template('Book_Seats.html')
 
-@app.route('/store_data/t/<id>',methods=['POST'])
+
+@app.route('/store_data/t/<id>', methods=['POST'])
 def store_theater_data(id):
     if request.method == 'POST':
         theater = request.form[f't{id}']
@@ -176,26 +179,31 @@ def store_theater_data(id):
         return render_template('Book_Seats.html')
     return render_template('Theater_List.html')
 
-@app.route('/store_seating',methods=['POST'])
+
+@app.route('/store_seating', methods=['POST'])
 def store_seating():
     if request.method == 'POST':
-        seates=request.form.getlist('tickets')
-        date=request.form['trip-start']
-        time=request.form['date']
-        amount=request.form['amount']
-        print(seates,date,time)
-        collection.find_one({'id':ticket_id})
-        collection.find_one({'movie':movie_name})
-        collection.find_one({'theater':screen_name})
+        seates = request.form.getlist('tickets')
+        date = request.form['trip-start']
+        time = request.form['date']
+        amount = request.form['amount']
+        print(seates, date, time)
+        collection.find_one({'id': ticket_id})
+        collection.find_one({'movie': movie_name})
+        collection.find_one({'theater': screen_name})
         collection.insert_one({'seates': seates})
         collection.insert_one({'date': date})
         collection.insert_one({'time': time})
         collection.insert_one({'amount': amount})
 
-        create_movie_ticket(ticket_id, movie_name, screen_name, seates, date, time, amount)
-        return send_file('movie_ticket.pdf',as_attachment=True)
+        create_movie_ticket(ticket_id, movie_name, screen_name, seates, date,
+                            time, amount)
+        return send_file('movie_ticket.pdf', as_attachment=True)
     return render_template('Book_Seats.html')
 
+@app.route('/<movie>')
+def movielist(movie):
+    return render_template(f'{movie}.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
